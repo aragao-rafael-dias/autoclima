1ce470899ae7837d59da6f99d0130d382067cd44
Rafael <rafaeldiasaragao@gmail.com>
2025-01-22 12:01:06 -0300

    Corrige configuração de STATICFILES_DIRS e STATIC_ROOT

--- climograma_site/settings.py
+++ climograma_site/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -116,8 +117,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / "staticfiles"]
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+STATIC_URL = '/static/'
+
+# Diretório onde arquivos estáticos coletados serão armazenados
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+# Diretórios adicionais para arquivos estáticos criados manualmente
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
+# Configurações adicionais para servir arquivos estáticos com Whitenoise
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
